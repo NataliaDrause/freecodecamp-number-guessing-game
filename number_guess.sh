ee2c5f06db80c56bd4a4a613c2df9402d45107fe
Natalia Drause <nataliadrause@gmail.com>
2025-10-01 11:30:04 -0400

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# When you run your script, you should prompt the user for a username with Enter your username:, and take a username as input. Your database should allow usernames that are 22 characters
+echo 'Enter your username:'
+read USERNAME
